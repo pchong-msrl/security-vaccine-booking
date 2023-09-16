dc1b489901c9d12d2de73201f2fd649fbc080df4
louolkol <louolkol@gmail.com>
2023-09-17 02:11:04 +0800

    able to route to two page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,40 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <button>Login</button>
-      <button>Vaccine Register</button>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/vaccine-register">Vaccine Register</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/login">
+            <LoginPage />
+          </Route>
+          <Route path="/vaccine-register">
+            <VaccineRegisterPage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
+}
+
+function LoginPage() {
+  return <h2>Login Page</h2>;
+}
+
+function VaccineRegisterPage() {
+  return <h2>Vaccine Register Page</h2>;
 }
 
 export default App;
