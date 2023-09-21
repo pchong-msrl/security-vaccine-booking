8ea494a8fa02506064f4e8c86832841d01c99271
alex <alex.wong@one2.cloud>
2023-09-21 22:39:36 +0800

    add login logout

--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -3,16 +3,17 @@ import React from "react";
 import {useAuth} from "./hook/useAuth";
 
 const NavBar = () =>{
-  const {token} = useAuth()
+  const {token,setToken} = useAuth()
    return <nav>
         <ul>
-            <li>
-                <Link to="/login">Login</Link>
-            </li>
+
             <li>
                 <Link to="/vaccine-register">Vaccine Register</Link>
             </li>
-            {token? <div>Admin Here</div>:<div>UnAuth</div> }
+            {token? <button onClick={()=>{setToken(undefined)}}>Log out</button>:
+                <li>
+                <Link to="/login">Login</Link>
+            </li> }
         </ul>
     </nav>
 }
