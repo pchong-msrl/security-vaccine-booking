970c482bd88bcc6c3374c378987c994a776407a8
louolkol <louolkol@gmail.com>
2023-09-21 22:55:16 +0800

    URL update

--- src/api/user/getAvailable.ts
+++ src/api/user/getAvailable.ts
@@ -1,8 +1,8 @@
 import config from "../../config";
 import axios from "axios";
 
-const getAvailable = async ()=>{
-    const url = new URL('/available',config.api.user)
-    const res = await axios.get(url.href)
-    return res.data
-}
+const getAvailable = async () => {
+  const url = new URL("/timeslots/available", config.api.user);
+  const res = await axios.get(url.href);
+  return res.data;
+};
--- src/config.ts
+++ src/config.ts
@@ -1,15 +1,14 @@
-
-interface IConfig{
-    api:{
-        admin:string
-        user:string
-    }
+interface IConfig {
+  api: {
+    admin: string;
+    user: string;
+  };
 }
 
- const config:IConfig = {
-    api:{
-        admin:process.env.API_ADMIN||"http://localhost:8000",
-        user:process.env.API_USER||"http://localhost:3000"
-    }
-}
-export default config
+const config: IConfig = {
+  api: {
+    admin: process.env.API_ADMIN || "http://localhost:8000",
+    user: process.env.API_USER || "http://localhost:5000",
+  },
+};
+export default config;
