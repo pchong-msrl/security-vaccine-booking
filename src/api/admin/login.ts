8d344e0ebf5efb49185cd1f216e03399fff4c2b1
louolkol <louolkol@gmail.com>
2023-09-24 19:10:06 +0800

    can login

--- src/api/admin/login.ts
+++ src/api/admin/login.ts
@@ -1,14 +1,16 @@
 import config from "../../config";
 import axios from "axios";
 
-interface ILogin{
-    username:string
-    password:string
+interface ILogin {
+  username: string;
+  password: string;
 }
-const login = async (input:ILogin)=>{
-    const url = new URL('/login',config.api.admin)
-    const res = await axios.post(url.href,input)
-    return res.data
-}
+const login = async (input: ILogin) => {
+  const url = new URL("api/users/login", config.api.admin);
+  const res = await axios.post(url.href, input);
+  //   console.log(res.data);
+  alert("login successfully");
+  return res.data;
+};
 
-export default login
+export default login;
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,34 +1,45 @@
-import React, {useState} from "react";
-import {useAuth} from "./hook/useAuth";
+import React, { useState } from "react";
+import { useAuth } from "./hook/useAuth";
 import login from "../api/admin/login";
 import contraner from "./contraner";
 import Contraner from "./contraner";
 
 function LoginPage() {
-  const {token,setToken}= useAuth()
-  const [username,setUsername] = useState('')
-  const [password,setPassword] = useState('')
+  const { token, setToken } = useAuth();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-  const handleLogin = async ()=>{
-    // const jwt = await login({username,password}) -> use api
-    const jwt = "asdsad"
-    if(jwt){
-      setToken(jwt)
+  const handleLogin = async () => {
+    const jwt = await login({ username, password });
+    // const jwt = "asdsad"
+    if (jwt) {
+      setToken(jwt);
     }
-  }
+  };
   return (
-      <Contraner>
-        <div style={{marginTop:"2rem"}}></div>
-        <input value={username} onChange={(e)=>setUsername(e.target.value)} placeholder={"Username"}/>
-        <div style={{marginTop:"2rem"}}></div>
-        <input value={password} onChange={(e)=>setPassword(e.target.value)} placeholder={"Password"}/>
-        <div style={{marginTop:"2rem"}}></div>
-        <button onClick={async ()=>{
-          await handleLogin()
-        }}>Login</button>
-      </Contraner>
-)
-
+    <Contraner>
+      <div style={{ marginTop: "2rem" }}></div>
+      <input
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        placeholder={"Username"}
+      />
+      <div style={{ marginTop: "2rem" }}></div>
+      <input
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder={"Password"}
+      />
+      <div style={{ marginTop: "2rem" }}></div>
+      <button
+        onClick={async () => {
+          await handleLogin();
+        }}
+      >
+        Login
+      </button>
+    </Contraner>
+  );
 }
 
 export default LoginPage;
